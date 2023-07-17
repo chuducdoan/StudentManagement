6a877bfa4e2119f5c099d0f508fe8ade169a30f6
DoanCD <chuducdoan3011@gmail.com>
2023-07-17 13:46:43 +0700

    fix bug route

--- student-management/src/App.tsx
+++ student-management/src/App.tsx
@@ -2,11 +2,11 @@ import { useAppSelector } from "app/hooks";
 import { darkTheme, lightTheme } from "common/style/themes";
 import { Loading } from "components";
 import { selectTheme } from "features/theme/themeSlice";
-import { DefaultLayout } from "layout";
-import { Suspense, lazy } from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import "./App.css";
+import { DefaultLayout } from "layout";
+import { Suspense, lazy } from "react";
 
 const Dashboard = lazy(() => import("pages/Dashboard"));
 const Login = lazy(() => import("pages/Login"));
@@ -20,16 +20,15 @@ function App() {
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <Router forceRefresh>
-        <Suspense fallback={getLoading()}>
-          <Switch>
-            <Route path={"/login"} component={Login} />
-            <DefaultLayout>
-              <Route path={"/dashboard"} component={Dashboard} />
-            </DefaultLayout>
-          </Switch>
-        </Suspense>
-      </Router>
+      <Suspense fallback={getLoading()}>
+        <Switch>
+          <Route exact path={"/"} children={<Login />} />
+          <Route exact path={"/login"} children={<Login />} />
+          <DefaultLayout>
+            <Route exact path={"/dashboard"} children={<Dashboard />} />
+          </DefaultLayout>
+        </Switch>
+      </Suspense>
     </ThemeProvider>
   );
 }
--- student-management/src/index.tsx
+++ student-management/src/index.tsx
@@ -7,17 +7,18 @@ import "i18n";
 import { Provider } from "react-redux";
 import { store } from "app/store";
 import GlobalStyle from "common/style/global-style";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
       <GlobalStyle />
     </Provider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
